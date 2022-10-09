11a299dfa80d399b62d0303b7553d9e3be418e22
Omar ElGamal <61190251+oae233@users.noreply.github.com>
2022-10-09 23:53:25 +0400

    Cleaned Code and Added Comments

--- myscript.js
+++ myscript.js
@@ -1,13 +1,15 @@
 const html = document.documentElement;
+// Referencing my canvas element so I can change the picture displayed within it
 const canvas = document.getElementById("scenery");
 const context = canvas.getContext("2d");
-console.log("hello AGAIN!");
+// Creating a variable for my framecount (the number of frames making up the background animation) to map against the scroll position
 const frameCount = 93;
-// const rgbval;
 const currentFrame = index => (
   `${index.toString().padStart(4, '0')}.jpg`
 )
 
+// Preloading the images so the webiste could have a better experience
+// The code I used here is from this tutorial: https://css-tricks.com/lets-make-one-of-those-fancy-scrolling-animations-used-on-apple-product-pages/
 const preloadImages = () => {
   for (let i = 1; i < frameCount; i++) {
     const img = new Image();
@@ -22,7 +24,7 @@ canvas.height=1080;
 
 // Create, load and draw the image
 const img = new Image()
-img.src = currentFrame(1); // we'll make this dynamic in the next step, for now we'll just load image 1 of our sequence
+img.src = currentFrame(1);
 img.onload=function(){
   context.drawImage(img, 0, 0);
 }
@@ -32,9 +34,10 @@ const updateImage = index => {
   context.drawImage(img, 0, 0);
 }
 
-let mainVideo = document.getElementById("mainvid");
+let mainVideo = document.getElementById("myvideo");
 let bod = document.getElementById("bod");
 
+//where the magic happens :) the code checks for the Y-scroll position and displays the correspodning frame
 window.addEventListener('scroll', () => {
   const scrollTop = html.scrollTop;
   const maxScrollTop = html.scrollHeight - window.innerHeight;
@@ -43,19 +46,25 @@ window.addEventListener('scroll', () => {
     frameCount - 1,
     Math.ceil(scrollFraction * frameCount)
   );
+  //I added a variable called rgbval to control the color of the text on the page, so it looks like the text is lighting up in response to the sunrise in the animation.
   const rgbval =  20 + (255- (255-(frameIndex*9)));
   requestAnimationFrame(() => updateImage(frameIndex + 1))
   bod.style.color = `rgb(${rgbval},${rgbval},${rgbval})`;
+  //the same variable is used to contorl the hooks opacity
+  hook.style.opacity = `${(10/rgbval)*100}%`
+
+  //checks if user has scrolled to the bottom of the page to display the video or not
   if (frameIndex > 91) {
-    document.getElementById( 'mainvid' ).style.display = 'flex';
-    // document.getElementById( 'mainvid' ).vid.play();
+    document.getElementById( 'myvideo' ).style.display = 'flex';
   } else {
-    document.getElementById( 'mainvid' ).style.display = 'none';
+    document.getElementById( 'myvideo' ).style.display = 'none';
   }
 });
+
+//calling the preload images function
 preloadImages();
 
-
+// This is to copy my email and the website link to the clipboard on click, and notify the user
 function myFunction() {
   // Get the text field
   var copyText = document.getElementById("myInput");
@@ -63,7 +72,20 @@ function myFunction() {
   // Select the text field
   copyText.select();
   copyText.setSelectionRange(0, 99999); // For mobile devices
-  document.getElementById( 'subhead' ).style.visibility = 'visible';
+  document.getElementById( 'notify' ).style.visibility = 'visible';
+
+   // Copy the text inside the text field
+  navigator.clipboard.writeText(copyText.value);
+
+}
+function myFunction2() {
+  // Get the text field
+  var copyText = document.getElementById("myInput2");
+
+  // Select the text field
+  copyText.select();
+  copyText.setSelectionRange(0, 99999); // For mobile devices
+  document.getElementById( 'notify' ).style.visibility = 'visible';
 
    // Copy the text inside the text field
   navigator.clipboard.writeText(copyText.value);
